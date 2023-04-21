eec11805a31d72b39eb55c49351f3ee35f750fcc
tazhibayda <200103210@stu.sdu.edu.kz>
2023-04-22 02:02:25 +0600

    not working

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/tazhibayda/DHL_API
 
 go 1.20
 
-require github.com/tazhibayda/testProject_Golang v0.0.0-20230415073255-bb7df5a5dc93
+require github.com/tazhibayda/testProject_Golang v0.0.0-20230420091450-938f24888708
--- main.go
+++ main.go
@@ -1,20 +1,48 @@
+//package main
+//
+//import (
+//	"fmt"
+//	"github.com/tazhibayda/DHL_API/dhl"
+//)
+//
+//const (
+//	Account  = ""
+//	Password = ""
+//	ApiURL   = "https://api-mock.dhl.com/mydhlapi/"
+//)
+//
+//func main() {
+//
+//	post := dhl.NewDHL(Account, Password, ApiURL)
+//
+//	fmt.Println(post)
+//
+//}
+
 package main
 
 import (
 	"fmt"
-	"github.com/tazhibayda/DHL_API/dhl"
-)
-
-const (
-	Account  = ""
-	Password = ""
-	ApiURL   = "https://api-mock.dhl.com/mydhlapi/"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
 
-	post := dhl.NewDHL(Account, Password, ApiURL)
+	url := "https://api-mock.dhl.com/mydhlapi/address-validate?type=SOME_STRING_VALUE&countryCode=SOME_STRING_VALUE"
 
-	fmt.Println(post)
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("Message-Reference", "d0e7832e-5c98-11ea-bc55-0242ac13")
+	req.Header.Add("Message-Reference-Date", "Wed, 21 Oct 2015 07:28:00 GMT")
+	req.Header.Add("Authorization", "Basic ZGVtby1rZXk6ZGVtby1zZWNyZXQ=")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	fmt.Println(res)
+	fmt.Println(string(body))
 
 }
